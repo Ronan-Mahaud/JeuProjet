e4f97e01f360542e554ecbbb7bca9a39f06d5f1d
ronan <95481680+Ronan-Mahaud@users.noreply.github.com>
2022-06-03 18:40:22 +0200

    Delete Solaria Demo Update 01.tsx

--- Solaria Demo Update 01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Solaria Demo Update 01" tilewidth="16" tileheight="16" tilecount="420" columns="28">
- <image source="Solaria Demo Update 01.png" trans="000000" width="448" height="240"/>
-</tileset>
