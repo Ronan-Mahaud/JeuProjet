37a3b023a74414263e97660f37df77a4ae11fb54
ronan <95481680+Ronan-Mahaud@users.noreply.github.com>
2022-06-03 18:40:45 +0200

    Delete house interior.tsx

--- house interior.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="house interior" tilewidth="16" tileheight="16" tilecount="775" columns="31">
- <image source="house interior.png" trans="ffffff" width="500" height="400"/>
-</tileset>
